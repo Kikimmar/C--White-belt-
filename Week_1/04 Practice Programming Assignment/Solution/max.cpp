f464fb43f97f0f89c349a483e80e791eee576486
user.name <Kikimmar.m@gmail.com>
2023-06-16 18:36:44 +0700

    p04- 08

---
+++ Week_1/04 Practice Programming Assignment/Solution/max.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+int a, b, c;
+cin >> a >> b;
+	if(b == 0){
+	cout << "Imposible\n";
+	}
+	else{
+	c = a / b;
+	cout << c << endl;
+	}
+return 0;
+}
